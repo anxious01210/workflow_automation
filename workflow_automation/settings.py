8c9e46d97b43e06ab9032a160cb72f6e1caa1732
anxious01210 <pinktheory21@gamil.com>
2025-08-06 20:17:20 +0300

    4th: core/ builder => CSS & JS Separation.

--- workflow_automation/settings.py
+++ workflow_automation/settings.py
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# ✅ Static files (CSS, JS, etc.)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # for global static files
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # used when running collectstatic
+
+# ✅ Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- workflow_automation/urls.py
+++ workflow_automation/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),  # Add this
 ]
+
+# ✅ Serve media and static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_title = "BISK Admin Portal"
 admin.site.site_header = "Welcome to BISK Admin Area"
